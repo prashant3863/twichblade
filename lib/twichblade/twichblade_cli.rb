b1238adb7f3f83087617af0c3b8a4b4c5d1fd341
Prashant Mittal <TheWhip@Prashants-MacBook-Pro.local>
2015-08-27 16:32:26 +0530

    Prompting for passsword

--- lib/twichblade/twichblade_cli.rb
+++ lib/twichblade/twichblade_cli.rb
@@ -4,5 +4,9 @@ module TwichBlade
     def get_username
       puts "username: "
     end
+
+    def get_password
+      puts "password: "
+    end
   end
 end
--- spec/twichblade/twichblade_cli_spec.rb
+++ spec/twichblade/twichblade_cli_spec.rb
@@ -4,9 +4,13 @@ module TwichBlade
   describe "TwichBladeCli" do
     let(:cli) { TwichBladeCli.new }
 
-    context "credentials" do
+    context "prompt for credential" do
       it "asks for username" do
         expect{ cli.get_username }.to output(/username:/).to_stdout
+      end
+
+      it "ask for password" do
+        expect{ cli.get_password }.to output(/password:/).to_stdout
       end
     end
   end
