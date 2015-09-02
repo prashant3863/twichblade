56bca4379a9ac386e525e3d6cc0724f3f2fc1c85
prashant <prashantee@iitj.ac.in>
2015-09-02 14:50:59 +0530

    refactored the cli specs for method calls rather than stdout

--- spec/twichblade/twichblade_cli_spec.rb
+++ spec/twichblade/twichblade_cli_spec.rb
@@ -5,6 +5,7 @@ module TwichBlade
     before(:all) {@dbconnection =  DatabaseConnection.new("twichblade_spec").connection}
     let(:cli) { TwichBladeCli.new(@dbconnection) }
     after (:each) { @dbconnection.exec("delete from user_info") }
+    let(:user) { User.new('prashant', 'foobar', @dbconnection) }
 
     it "displays the index page" do
       expect{ cli.index_page }.to output(/1. Signup\n2. Login/).to_stdout
@@ -27,16 +28,20 @@ module TwichBlade
       it "calls signup on user when 1 is selcted" do
         input = "1"
         allow(Kernel).to receive(:gets).and_return('prashant', 'foobar')
-        expect{ cli.delegate(input) }.to output(/Signup sucessfull/).to_stdout
+        allow(User).to receive(:new).and_return(user)
+        expect(user).to receive(:signup)
+        cli.delegate(input)
       end
 
       it "calls login on user when 2 is selcted" do
         input = "2"
         allow(Kernel).to receive(:gets).and_return('prashant', 'foobar')
-        expect{ cli.delegate(input) }.to output(/Incorrect Username or Password/).to_stdout
+        allow(User).to receive(:new).and_return(user)
+        expect(user).to receive(:login)
+        cli.delegate(input)
       end
 
-      it "invalid option message" do
+      it "displays invalid option message" do
         input = "3"
         allow(Kernel).to receive(:gets).and_return('prashant', 'foobar')
         expect{ cli.delegate(input) }.to output(/Please enter a valid option/).to_stdout
@@ -45,11 +50,10 @@ module TwichBlade
 
     context "login delegate" do
       before (:each) { @dbconnection.exec("insert into user_info (username, password) values ('prashant', 'foobar')")}
-      it "Logs out the user when logout option is selected" do
-        user_1 = User.new('prashant', 'foobar', @dbconnection)
-        allow(User).to receive(:new).and_return(user_1)
+      it "logs out the user when logout option is selected" do
+        allow(User).to receive(:new).and_return(user)
         cli.delegate('2')
-        expect(user_1).to receive(:logout)
+        expect(user).to receive(:logout)
         cli.login_delegate('1')
       end
     end
